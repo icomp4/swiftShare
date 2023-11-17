17fa1baf854262f4d11cb2d4f2b04e16f8de864a
isaiah compere <rewrittenzip@gmail.com>
2023-11-17 02:07:55 -0500

    starting to implement frontend

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 	"swiftShare/database"
@@ -16,8 +17,19 @@ func main() {
 	if err := database.DB.Ping(); err != nil {
 		log.Fatal(err)
 	}
-
 	mux := http.NewServeMux()
+
+	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+
+	templates := template.Must(template.ParseFiles("static/main.html"))
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// Render the HTML template
+		if err := templates.ExecuteTemplate(w, "main.html", nil); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
 	signupHandler := middleware.RequireAuth(middleware.Logger(http.HandlerFunc(handlers.SignUp)))
 	loginHandler := middleware.RequireAuth(middleware.Logger(http.HandlerFunc(handlers.SignIn)))
 	logoutHandler := middleware.Logger(http.HandlerFunc(handlers.LogOut))
@@ -25,14 +37,13 @@ func main() {
 	requestEmailHandler := middleware.RequireAuth(middleware.Logger(http.HandlerFunc(handlers.RequestEmail)))
 	updatePassswordHandler := middleware.RequireAuth(middleware.Logger(http.HandlerFunc(handlers.UpatePassword)))
 
-	
 	mux.Handle("/signup", signupHandler)
 	mux.Handle("/login", loginHandler)
 	mux.Handle("/logout", logoutHandler)
 	mux.Handle("/delete", deleteHandler)
 	mux.Handle("/request", requestEmailHandler)
 	mux.Handle("/password/update", updatePassswordHandler)
-	
+
 	if err := http.ListenAndServe(":8080", mux); err != nil {
 		log.Fatal(err)
 	}
